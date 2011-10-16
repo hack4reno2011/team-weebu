0a178a67da48704ed932ffe99c0ffa4e058b1d2e
unknown <owner@.(none)>
2011-10-16 12:15:25 -0700

    Final Code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   before_filter :authenticate, :only => [:new, :join]
-
+  TIMES = ['7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00am',
+          '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm']
   def new
      @game = Game.new
      @title = "Create Game"
